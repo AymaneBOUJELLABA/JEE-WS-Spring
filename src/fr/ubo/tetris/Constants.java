09cd697e86d632094099f791b24d881f0806082d
aymaneBOUJELLABA <aymane.boujellaba@gmail.com>
2021-10-06 11:25:16 +0200

    First Commit

---
+++ src/fr/ubo/tetris/Constants.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package fr.ubo.tetris;
+
+
+/**
+ * Project : Tetris <br/>
+ * Package : fr.ubo.tetris <br/>
+ * Nom : Constants <br/>
+ *
+ * Description : TODO <br/>
+ * @author BOUJELLABA Aymane
+ */
+public final class Constants {
+
+  /**
+   * Largeur de la fenêtre de jeu
+   */
+  public static final int BOARD_WIDTH = 10;
+
+  /**
+   * Hauteur de la fenêtre de jeu
+   */
+  public static final int BOARD_HEIGHT = 22;
+
+  /**
+   * Constructeur
+   */
+  private Constants() {
+  }
+}
---
+++ src/fr/ubo/tetris/Tetrominoes.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package fr.ubo.tetris;
+
+/**
+ * Project : Tetris <br/>
+ * Package : fr.isen.tetris <br/>
+ * Nom : Terominoes <br/>
+ * 
+ * Description : Liste finie des formes possibles<br/>
+ * 
+ * @author BOUJELLABA Aymane
+ */
+public enum Tetrominoes {
+  NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredLShape, NewShape,
+}
